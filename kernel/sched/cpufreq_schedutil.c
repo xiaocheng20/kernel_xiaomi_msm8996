f26e26a32b4603373702ae5b5bdfe3124be96e52
Viresh Kumar <viresh.kumar@linaro.org>
2018-05-09 16:05:24 +0530

    UPSTREAM: cpufreq: schedutil: Don't set next_freq to UINT_MAX
    The schedutil driver sets sg_policy->next_freq to UINT_MAX on certain
    occasions to discard the cached value of next freq:
    - In sugov_start(), when the schedutil governor is started for a group
      of CPUs.
    - And whenever we need to force a freq update before rate-limit
      duration, which happens when:
      - there is an update in cpufreq policy limits.
      - Or when the utilization of DL scheduling class increases.
    
    In return, get_next_freq() doesn't return a cached next_freq value but
    recalculates the next frequency instead.
    
    But having special meaning for a particular value of frequency makes the
    code less readable and error prone. We recently fixed a bug where the
    UINT_MAX value was considered as valid frequency in
    sugov_update_single().
    
    All we need is a flag which can be used to discard the value of
    sg_policy->next_freq and we already have need_freq_update for that. Lets
    reuse it instead of setting next_freq to UINT_MAX.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: joshuous <joshuous@gmail.com>
    
    Conflicts:
    	kernel/sched/cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -89,6 +89,9 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 {
 	s64 delta_ns;
 
+	if (sg_policy->work_in_progress)
+		return false;
+
 	if (unlikely(sg_policy->need_freq_update))
 		return true;
 
@@ -327,8 +330,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		 * Do not reduce the frequency if the CPU has not been idle
 		 * recently, as the reduction is likely to be premature then.
 		 */
-		if (busy && next_f < sg_policy->next_freq &&
-		    sg_policy->next_freq != UINT_MAX) {
+		if (busy && next_f < sg_policy->next_freq) {
 			next_f = sg_policy->next_freq;
 
 			/* Reset cached freq as next_freq has changed */
