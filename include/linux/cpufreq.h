dcac81e2700cf96ada03ebd51c87c6a85f7cd91c
TR-76 <tr-76@gmx.at>
2018-11-17 17:16:23 +0100

    add darkness cpu governor
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -552,6 +552,9 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
