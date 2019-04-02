6ada38b3c8d4ced3c9f91357c846a965539e9c6c
mady51 <root@instance-1.us-east1-b.c.polar-cyclist-235904.internal>
2019-04-02 07:56:00 +0000

    some sched params tweaks

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -50,8 +50,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 10000000ULL;
-unsigned int normalized_sysctl_sched_latency = 10000000ULL;
+unsigned int sysctl_sched_latency = 4000000ULL;
+unsigned int normalized_sysctl_sched_latency = 4000000ULL;
 
 unsigned int sysctl_sched_sync_hint_enable = 1;
 unsigned int sysctl_sched_cstate_aware = 1;
