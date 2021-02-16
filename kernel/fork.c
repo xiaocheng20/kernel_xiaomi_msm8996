6227d1bcfff3551db62dcf393bb6819e8dbda4fb
xiaocheng20 <yuchengtan0@gmail.com>
2021-02-16 22:21:03 +0800

    No changes

--- kernel/fork.c
+++ kernel/fork.c
@@ -1697,7 +1697,7 @@ long do_fork(unsigned long clone_flags,
 	/* Boost CPU to the max for 1250 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
 		cpu_input_boost_kick_max(1250);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW,1250);
 	}
 
 	/*
