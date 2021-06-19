aeedb607e10280148b31942251ec81c02e0a91c2
xiaocheng20 <yuchengtan0@gmail.com>
2021-06-19 15:42:57 +0800

    energy model changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CROSS_COMPILE=~/Desktop/Toolchain/google_gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/Toolchain/google_gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 export ARCH=arm64
 export SUBARCH=arm64
