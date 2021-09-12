450dcb882297d8cbc4e75fe4f6133a63dcf34d1a
xiaocheng20 <yuchengtan0@gmail.com>
2021-09-12 16:55:05 +0800

    asm-generic:300hz

--- include/asm-generic/param.h
+++ include/asm-generic/param.h
@@ -4,7 +4,7 @@
 #include <uapi/asm-generic/param.h>
 
 # undef HZ
-# define HZ		1000		/* Internal kernel timer frequency */
-# define USER_HZ	1000		/* some user interfaces are */
+# define HZ		300		/* Internal kernel timer frequency */
+# define USER_HZ	300		/* some user interfaces are */
 # define CLOCKS_PER_SEC	(USER_HZ)       /* in "ticks" like times() */
 #endif /* __ASM_GENERIC_PARAM_H */
